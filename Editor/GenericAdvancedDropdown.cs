2532e38e203f2bdb4c96008aed56e33a428c5c32
bennykok <itechbenny@gmail.com>
2021-06-19 17:29:34 +0800

    fix: updated advanced dropdown

--- Editor/GenericAdvancedDropdown.cs
+++ Editor/GenericAdvancedDropdown.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using UnityEditor;
 using UnityEditor.IMGUI.Controls;
 using UnityEngine;
@@ -15,11 +17,89 @@ namespace BennyKok.ToolbarButtons
         private Dictionary<int, Action> idToActionMap = new Dictionary<int, Action>();
         private string title = "Items";
 
+        #if UNITY_2021_2_OR_NEWER
+        static PropertyInfo menuItemsField = typeof(GenericMenu).GetProperty("menuItems",
+            BindingFlags.NonPublic |
+            BindingFlags.Instance);
+        #else
+        static FieldInfo menuItemsField = typeof(GenericMenu).GetField("menuItems",
+            BindingFlags.NonPublic |
+            BindingFlags.Instance);
+        #endif
+
+        static FieldInfo menuItemContentField = typeof(GenericMenu).GetNestedType("MenuItem", BindingFlags.NonPublic |
+            BindingFlags.Instance)?.GetField("content",
+            BindingFlags.Public |
+            BindingFlags.Instance);
+
+        static FieldInfo menuItemMenuFunctionField = typeof(GenericMenu).GetNestedType("MenuItem", BindingFlags.NonPublic |
+            BindingFlags.Instance)?.GetField("func",
+            BindingFlags.Public |
+            BindingFlags.Instance);
+
+        static FieldInfo menuItemMenuFunction2Field = typeof(GenericMenu).GetNestedType("MenuItem", BindingFlags.NonPublic |
+            BindingFlags.Instance)?.GetField("func2",
+            BindingFlags.Public |
+            BindingFlags.Instance);
+
+        static FieldInfo menuItemUserDataField = typeof(GenericMenu).GetNestedType("MenuItem", BindingFlags.NonPublic |
+            BindingFlags.Instance)?.GetField("userData",
+            BindingFlags.Public |
+            BindingFlags.Instance);
+
+        static FieldInfo menuItemSeparatorField = typeof(GenericMenu).GetNestedType("MenuItem", BindingFlags.NonPublic |
+            BindingFlags.Instance)?.GetField("separator",
+            BindingFlags.Public |
+            BindingFlags.Instance);
+
         public GenericAdvancedDropdown(AdvancedDropdownState state) : base(state) { }
         public GenericAdvancedDropdown() : base(new AdvancedDropdownState()) { }
         public GenericAdvancedDropdown(string title) : base(new AdvancedDropdownState())
         {
             this.title = title;
+        }
+
+        public GenericAdvancedDropdown(string title, GenericMenu menu) : base(new AdvancedDropdownState())
+        {
+            this.title = title;
+
+            var allItems = menuItemsField.GetValue(menu) as IEnumerable;
+            foreach (var item in allItems)
+            {
+                var content = menuItemContentField.GetValue(item) as GUIContent;
+                var function = menuItemMenuFunctionField.GetValue(item) as GenericMenu.MenuFunction;
+                var function2 = menuItemMenuFunction2Field.GetValue(item) as GenericMenu.MenuFunction2;
+                var userData = menuItemUserDataField.GetValue(item);
+                var separator = menuItemSeparatorField.GetValue(item) as bool?;
+
+                // Debug.Log(menuItemMenuFunctionField.GetValue(item).GetType());
+                // Debug.Log(menuItemMenuFunction2Field.GetValue(item).GetType());
+
+                if (string.IsNullOrEmpty(content.text)) continue;
+                if (separator.HasValue && separator.Value) continue;
+                if (function == null && function2 == null) continue;
+
+                AddItem(content.text, () =>
+                {
+                    if (function != null) function();
+                    if (function2 != null) function2(userData);
+                });
+            }
+        }
+
+        public void Dropdown(Rect rect, int minLineCount = -1)
+        {
+            var originalHeight = rect.height;
+            Vector2 size = new Vector2(rect.width, 100);
+
+            if (minLineCount > 0)
+            {
+                size.y = minLineCount * EditorGUIUtility.singleLineHeight;
+            }
+            this.minimumSize = size;
+
+            var r = new Rect(rect.position - new Vector2(0, size.y - originalHeight), size);
+            Show(r);
         }
 
         public void ShowAsContext(int minLineCount = -1)
@@ -29,10 +109,10 @@ namespace BennyKok.ToolbarButtons
             if (minLineCount > 0)
             {
                 size.y = minLineCount * EditorGUIUtility.singleLineHeight;
-                this.minimumSize = size;
             }
-
-            var r = new Rect(Event.current.mousePosition - new Vector2(size.x / 2, size.y), size);
+            this.minimumSize = size;
+            var p = GUIUtility.ScreenToGUIPoint(Event.current.mousePosition) - new Vector2(size.x / 2, size.y);
+            var r = new Rect(GUIUtility.GUIToScreenPoint(p), size);
             Show(r);
         }
 
@@ -69,7 +149,7 @@ namespace BennyKok.ToolbarButtons
                     lastItem = child;
                 }
 
-                if (lastItem != null)
+                if (lastItem != null && !idToActionMap.ContainsKey(lastItem.id))
                 {
                     idToActionMap.Add(lastItem.id, item.Value);
                 }
